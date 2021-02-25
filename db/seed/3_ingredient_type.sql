9745fab15b6ca4fbf852e0233b2e69c8de3a63be
David Innes <david@mobalyticshq.com>
2021-02-25 13:28:53 +0100

    init

---
+++ db/seed/3_ingredient_type.sql
@@ -0,0 +1,11 @@
+INSERT INTO ingredient_type(name)
+VALUES ('Vegetable'),
+       ('Fruit'),
+       ('Spice'),
+       ('Herb'),
+       ('Cereal'),
+       ('Pulse'),
+       ('Fat'),
+       ('Nut'),
+       ('Seed'),
+       ('Sauce');
